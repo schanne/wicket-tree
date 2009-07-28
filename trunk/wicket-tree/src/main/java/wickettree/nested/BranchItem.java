2dc58770814a5f861bb920530c6658a3ac4ba238
sven@meiers.net <sven@meiers.net@01549cd0-7858-11de-a381-6b0c0bf21917>
2009-07-28 19:47:19 +0000

    formatted to wicket standards

--- trunk/wicket-tree/src/main/java/wickettree/nested/BranchItem.java
+++ trunk/wicket-tree/src/main/java/wickettree/nested/BranchItem.java
@@ -27,28 +27,35 @@ import org.apache.wicket.model.IModel;
  * 
  * @author Sven Meier
  */
-public final class BranchItem<T> extends Item<T> {
+public final class BranchItem<T> extends Item<T>
+{
 
 	private static final long serialVersionUID = 1L;
 
-	public BranchItem(String id, int index, IModel<T> model) {
+	public BranchItem(String id, int index, IModel<T> model)
+	{
 		super(id, index, model);
 
 		setOutputMarkupId(true);
 	}
 
 	@Override
-	protected void onComponentTag(ComponentTag tag) {
+	protected void onComponentTag(ComponentTag tag)
+	{
 		super.onComponentTag(tag);
 
-		if (isLast()) {
+		if (isLast())
+		{
 			tag.put("class", "branch-last");
-		} else {
+		}
+		else
+		{
 			tag.put("class", "branch");
 		}
 	}
 
-	private boolean isLast() {
+	private boolean isLast()
+	{
 		return getIndex() == getParent().size() - 1;
 	}
 }
