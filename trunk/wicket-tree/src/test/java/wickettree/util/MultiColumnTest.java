7255cfb23aba2e63845704ac85d8a8d6b3ef8d20
sven@meiers.net <sven@meiers.net@01549cd0-7858-11de-a381-6b0c0bf21917>
2011-04-07 16:18:00 +0000

    let MultiColumn support arbitrary column implementations (not restricted to AbstractColumn)

--- trunk/wicket-tree/src/test/java/wickettree/util/MultiColumnTest.java
+++ trunk/wicket-tree/src/test/java/wickettree/util/MultiColumnTest.java
@@ -15,38 +15,96 @@
  */
 package wickettree.util;
 
+import java.io.Serializable;
+
 import junit.framework.TestCase;
 
 import org.apache.wicket.extensions.markup.html.repeater.data.grid.ICellPopulator;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.AbstractColumn;
+import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.repeater.Item;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
+import org.apache.wicket.util.tester.WicketTester;
 
 /**
  * Test for {@link MultiColumn}.
  */
 public class MultiColumnTest extends TestCase
 {
-	private boolean populated = false;
+	
+	@SuppressWarnings("unused")
+	private WicketTester tester;
 
-	public void test() throws Exception
+	@Override
+	protected void setUp() throws Exception
 	{
-		AbstractColumn<String> column = new AbstractColumn<String>(Model.of("test"), "sortTest")
+		tester = new WicketTester();
+	}
+	
+	public void testColumn() throws Exception
+	{
+		final Label label = new Label("test");
+		
+		AbstractColumn<String> column = new AbstractColumn<String>(Model.of("header"), "sort")
 		{
 			public void populateItem(Item<ICellPopulator<String>> cellItem, String componentId,
 					IModel<String> rowModel)
 			{
-				populated = true;
+				cellItem.add(label);
+			}
+			
+			@Override
+			public String getCssClass()
+			{
+				return "css";
 			}
 		};
 
-		MultiColumn<String> multi = new MultiColumn<String>(String.class, column);
+		MultiColumn<Serializable> multi = new MultiColumn<Serializable>(String.class, column);
 
-		assertEquals("test", multi.getDisplayModel().getObject());
-		assertEquals("sortTest", multi.getSortProperty());
+		assertEquals("header", multi.getHeader("test").getDefaultModelObject());
+		assertEquals("sort", multi.getSortProperty());
+		assertEquals("css", multi.getCssClass());
 
-		multi.populateItem(null, "testId", Model.of(new String()));
-		assertTrue(populated);
+		Item<ICellPopulator<Serializable>> item = new Item<ICellPopulator<Serializable>>("container", 0);
+		multi.populateItem(item, "testId", new Model<Serializable>(new String()));
+		assertEquals(label, item.get(0));
+	}
+
+	public void testDisplayModelSortPropertyColumn() throws Exception
+	{
+		final Label label = new Label("test");
+		
+		AbstractColumn<String> column = new AbstractColumn<String>(Model.of("header"), "sort")
+		{
+			public void populateItem(Item<ICellPopulator<String>> cellItem, String componentId,
+					IModel<String> rowModel)
+			{
+				cellItem.add(label);
+			}
+			
+			@Override
+			public String getCssClass()
+			{
+				return "css";
+			}
+		};
+
+		MultiColumn<Serializable> multi = new MultiColumn<Serializable>(Model.of("header2"), "sort2", String.class, column) {
+			@Override
+			public String getCssClass()
+			{
+				return "css2";
+			}
+		};
+
+		assertEquals("header2", multi.getHeader("test").getDefaultModelObject());
+		assertEquals("sort2", multi.getSortProperty());
+		assertEquals("css2", multi.getCssClass());
+
+		Item<ICellPopulator<Serializable>> item = new Item<ICellPopulator<Serializable>>("container", 0);
+		multi.populateItem(item, "testId", new Model<Serializable>(new String()));
+		assertEquals(label, item.get(0));
 	}
 }
